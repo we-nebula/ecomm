0e7fcab2bce48b4b216855e0f4164e3f1d9bd006
Kruti <kruti2511@gmail.com>
2019-02-26 17:52:41 +0530

    Some New Modules

---
+++ development/we-nebula/ecomm/src/Model/Media.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace nebula\we\Model;
+
+
+class Media extends \nebula\we\Model {
+	
+	public $table='media';
+	public $caption ="Media";
+
+    public $acl_type='Media';
+
+    public $actions = [
+        'Active'=>['view','edit','delete','deactivate'],
+        'InActive'=>['view','edit','delete','activate'],
+    ];
+
+	public function init(){
+        parent::init();
+
+        $this->hasMany('MediaDescription',new \nebula\we\Model\MediaDescription);
+        $this->hasMany('ProductAttachmentMap',new \nebula\we\Model\ProductAttachmentMap);
+
+        $this->addFields([
+            ['name'],
+            ['file_url'],
+        	['meta'],
+        	['mime_type'],
+            ['status','enum'=>array_keys($this->actions)],
+        ]);
+
+        (new \nebula\we\Migration\MySQL($this))->migrate();
+
+    }
+}
