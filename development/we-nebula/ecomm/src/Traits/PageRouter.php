e4c50bde5e09bbc4eaaa175d7111d315b73463aa
Gowrav Viswhakarma <gowravvishwakarma@gmail.com>
2019-02-27 15:01:10 +0530

    updated intro and page URl now takes all other arguments also as for $app->url()

--- development/we-nebula/ecomm/src/Traits/PageRouter.php
+++ development/we-nebula/ecomm/src/Traits/PageRouter.php
@@ -4,15 +4,22 @@ namespace nebula\we\Traits;
 /**
  * This trait adds the most simple routing facility to your app, Yep, it's ugly but works
  * just add this trait in your APP
- * call $this->route() at the last in your init() function
- * and use like this (yes, its always index in url)
- * $menu = $this->app->layout->menu->addMenu('System');
- * $menu->addItem('Employee',$this->app->url(['index','page'=>'nebula\we\page\employees']));
  * 
- * This way ane class defined by page will be added in app and you will just be creating pages as class
- * for simplicity, we have a Page class that extends View (just empty)
- * and in 'page' folder in root we create extra pages like 'class employees extends Page' (from above example)
- * --- This will surely save you a lot of time
+ * call $this->route('page',['your\app\namespace','or\other\paths']) at the last in your init() function
+ * 
+ * $menu = $this->app->layout->menu->addMenu('System');
+ * $menu->addItem('Employee',$this->app->pageURL('employees']));
+ * 
+ * employee class will be searched as your\app\namespace\employees or or\other\paths\employees
+ * 
+ * usually it is sugested to create 'path' folder in your app src and create all page classes in there, you can use folder structure
+ * like $menu->addItem('Employee',$this->app->pageURL('employees/invoices']));
+ * 
+ * class employees or invoices here extends \atk4\ui\Page class
+ * while \atk4\ui\Page is with upper case P, you are sugestted to use page folder and classes in that folders
+ * all in lowercases
+ * 
+ * 
  */
 
 Trait PageRouter{
@@ -38,7 +45,7 @@ Trait PageRouter{
 		}
     }
 
-    function pageUrl($page_class){
-    	return $this->app->url(['index',$this->app_page_uri_identifier=>str_replace("\\", '-', $page_class)]);
+    function pageUrl($page_class,$needRequestUri = false, $extra_args = []){
+    	return $this->app->url(['index',$this->app_page_uri_identifier=>str_replace("\\", '-', $page_class)],$needRequestUri, $extra_args);
     }
 }
