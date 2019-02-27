c8979a372d06ca74001d6abcb909953ab62fe457
Gowrav Viswhakarma <gowravvishwakarma@gmail.com>
2019-02-27 12:54:42 +0530

    Updtaed Page router

--- development/we-nebula/ecomm/src/Traits/PageRouter.php
+++ development/we-nebula/ecomm/src/Traits/PageRouter.php
@@ -16,21 +16,29 @@ namespace nebula\we\Traits;
  */
 
 Trait PageRouter{
+	
 	public $page_locations = [];
+	public $app_page_uri_identifier='page';
 
 	function router($page_var='page', $page_locations=[]){
+		
 		$this->page_locations = $page_locations;
+		$this->app_page_uri_identifier = $page_var;
 
         if($page = $this->app->stickyGet($page_var)){
         	$page = str_replace('-', '\\', $page);
         	foreach ($this->page_locations as $loc) {
         		$page_path = '\\'.$loc.'\\'.$page;
+    			if(!class_exists($page_path,true) && !next ($this->page_locations)){
+					throw new \atk4\ui\Exception(['Class Not Found','class'=>$page_path,'attempted_location'=>$this->page_locations]);
+        			$this->terminate('asdas');
+    			}
 				$this->app->add(new $page_path);
         	}
 		}
     }
 
     function pageUrl($page_class){
-    	return $this->app->url(['index','page'=>str_replace("\\", '-', $page_class)]);
+    	return $this->app->url(['index',$this->app_page_uri_identifier=>str_replace("\\", '-', $page_class)]);
     }
 }
